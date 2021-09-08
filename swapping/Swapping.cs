d8836f1b63322b3536a886943aedbab92a37ec0b
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-08 09:31:56 +0530

    Adding Swapping Project

---
+++ swapping/Swapping.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swapping
+{
+    class Swapping
+    {
+        
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("Before Swapping value of a={0},b={1}", a, b);
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("After Swapping value of a={0},b={1}", a, b);
+            Console.ReadLine();
+
+           
+        }
+    }
+}
